4bb93d99afd20238476274e2b5ed1c3cf920bed5
gugewu <gugewu667@gmail.com>
2026-09-05 17:32:35 +0800

    Update uprobe.rs

--- frame-analyzer/src/uprobe.rs
+++ frame-analyzer/src/uprobe.rs
@@ -43,13 +43,20 @@ impl UprobeHandler {
         let program: &mut UProbe = bpf.program_mut("frame_analyzer_ebpf").unwrap().try_into()?;
         program.load()?;
         program.attach(
-            Some("_ZN7android7Surface11queueBufferEP19ANativeWindowBufferi"),
+            Some("_ZN7android7Surface11queueBufferERKNS_2spINS_13GraphicBufferEEERKNS1_INS_5FenceEEEPNS_24SurfaceQueueBufferOutputE"),
             0,
             "/system/lib64/libgui.so",
             Some(pid),
         ).or_else(|_e1| {
             program.attach(
-                Some("_ZN7android7Surface11queueBufferEP19ANativeWindowBufferiPNS_24SurfaceQueueBufferOutputE"),
+                Some("_ZN7android7Surface11queueBufferERKNS_2spINS_13GraphicBufferEEERKNS_23SurfaceQueueBufferInputEPNS_24SurfaceQueueBufferOutputE"),
+                0,
+                "/system/lib64/libgui.so",
+                Some(pid),
+            )
+        }).or_else(|_e2| {
+            program.attach(
+                Some("_ZN7android7Surface16hook_queueBufferEP13ANativeWindowP19ANativeWindowBufferi"),
                 0,
                 "/system/lib64/libgui.so",
                 Some(pid),
